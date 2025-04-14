366a2ddd71f4fe35bf7b5d3a694312c0868643a6
Martin Zeitler <syslogic@users.noreply.github.com>
2025-04-14 02:02:11 +0200

    sudoers.d drop-in config fixed; MODULE.bazel.lock needs be removed, to change dependency versions.

--- docker/rpm-builder/scripts/patch_zlib.sh
+++ docker/rpm-builder/scripts/patch_zlib.sh
@@ -9,4 +9,6 @@ SEARCH='bazel_dep(name = "zlib", version = "1.3.1.bcr.3")'
 REPLACE='bazel_dep(name = "zlib", version = "1.3.1.bcr.4")'
 cat "${CVD_DIR}/MODULE.bazel" | sed -e "s/${SEARCH}/${REPLACE}/" > "${CVD_DIR}/MODULE.patched"
 mv "${CVD_DIR}/MODULE.patched" "${CVD_DIR}/MODULE.bazel"
-echo "Patched: ${CVD_DIR}/MODULE.bazel: zlib@1.3.1.bcr.4"
+echo "RHEL patch: base/cvd/MODULE.bazel changed from zlib@1.3.1.bcr.3 to zlib@1.3.1.bcr.4"
+rm "${CVD_DIR}/MODULE.bazel.lock"
+echo "RHEL patch: base/cvd/MODULE.bazel.lock also removed"
